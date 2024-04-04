25acf9b375a5c034a59898f934a74de97a6f3aa3
Badr Almadhi <badr.almadhi95@gmail.com>
2024-04-03 23:58:11 -0600

    added seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,18 @@
 // create json folders and then seed
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+
+const seedData = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    
+}
