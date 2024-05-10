eb24bacddfa2b1f6684600aed4eacfcc2a5530e9
Badr Almadhi <badr.almadhi95@gmail.com>
2024-05-10 12:00:07 -0600

    added styling

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,5 +1,25 @@
 const commentFromHandler = async function (event) {
-event.preventDefault();
+  event.preventDefault();
 
-const blog_id = document.querySelector('')
-}
+  const blog_id = document.getElementById("new-comment").value.trim();
+  const commentDescription = document.getElementById('comment-description').value.trim();
+
+  if (commentDescription) {
+    
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        blog_id,
+        commentDescription,
+      }),
+      headers: {
+        content_Type: "application/json",
+      },
+    });
+    document.location.reload();
+  }
+};
+
+document
+  .querySelector('.add-comment')
+  .addEventListener("submit", commentFromHandler);
